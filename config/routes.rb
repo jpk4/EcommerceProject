5a2ca6b9c5030dbbda43cb763f96662b110c437a
jpk4 <jkleysen@academic.rrc.ca>
2020-04-03 13:38:48 -0500

    Change root routing to products index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
-  get 'categories/index'
-  get 'delivery_persons/index'
-  get 'payments/index'
-  get 'customers/index'
-  get 'orders/index'
+  root to: 'products#index'
+
+  resources 'products', only: %i[index show]
+  resources 'categories', only: %i[index]
+  resources 'delivery_persons', only: %i[index]
+  resources 'payments', only: %i[index]
+  resources 'customers', only: %i[index]
+  resources 'orders', only: %i[index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
