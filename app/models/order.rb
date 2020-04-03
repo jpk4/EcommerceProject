097b91295c01c9d3a3bd528f7bfb40a135c45eb0
jpk4 <jkleysen@academic.rrc.ca>
2020-04-03 12:35:28 -0500

    Added has_many relationships for foreign keys and join tables

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 class Order < ApplicationRecord
   belongs_to :customer
   belongs_to :payment
+  has_many :order_products
+  has_many :products, through: :order_products
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class Payment < ApplicationRecord
+  has_many :orders
+  has_many :customers, through: :orders
 end
