583bf4973d52eefc2640957c7a1f0751db405841
jpk4 <jkleysen@academic.rrc.ca>
2020-04-03 12:13:32 -0500

    Create category model and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_03_171148) do
+ActiveRecord::Schema.define(version: 2020_04_03_171308) do
+
+  create_table "categories", force: :cascade do |t|
+    t.integer "category_id"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "customers", force: :cascade do |t|
     t.integer "customer_num"
