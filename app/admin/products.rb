33a2d9ac03c96b935546028cfca3d8e997d74ef0
jpk4 <jkleysen@academic.rrc.ca>
2020-04-03 15:07:07 -0500

    Add active admin for categories and products

---
+++ app/admin/products.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+ActiveAdmin.register Product do
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :product_id, :sku, :qty_on_hand, :current_price, :description, :image, :category_id, :name
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:product_id, :sku, :qty_on_hand, :current_price, :description, :image, :category_id, :name]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+end
