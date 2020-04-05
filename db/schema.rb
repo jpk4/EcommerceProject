63e725905d48a504652a9ded07d94b5243866918
jpk4 <jkleysen@academic.rrc.ca>
2020-04-05 15:52:42 -0500

    Add size column to products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_03_194037) do
+ActiveRecord::Schema.define(version: 2020_04_05_205134) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -110,6 +110,7 @@ ActiveRecord::Schema.define(version: 2020_04_03_194037) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.string "size"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
