960b2d1fe34a9630c47f90b612bed174c80ca37f
jpk4 <jkleysen@academic.rrc.ca>
2020-04-05 22:35:07 -0500

    Change validations based on sku column type changing

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,8 +5,7 @@ class Product < ApplicationRecord
   has_many :order_products
   has_many :orders, through: :order_products
 
-  validates :sku, :current_price, :description, :name, presence: true
+  validates :sku, :current_price, :name, presence: true
   validates :qty_on_hand, numericality: { only_integer: true }
-  validates :sku, numericality: { only_integer: true }
   validates :current_price, format: { with: /\A\d+(?:\.\d{2})?\z/ }, numericality: { greater_than_or_equal_to: 0 }
 end
