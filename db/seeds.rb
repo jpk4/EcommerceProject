028fd965f3b046dbd31e768a2eeac408db817307
jpk4 <jkleysen@academic.rrc.ca>
2020-04-07 04:44:45 -0500

    Clear out commented code from seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,30 +17,6 @@ Payment.destroy_all
 csv_file = Rails.root + 'db/groceries.csv'
 products = CSV.parse(File.read(csv_file), headers: true)
 
-# products.each do |parent_category|
-#   parent = products['parent category']
-
-#   Category.where(name: parent_category).first_or_create
-#   parent_category.each do |_sub_categories|
-#     sub_category = products['category']
-
-#     Category.where(name: sub_category).first_or_create(name: products['category'], parent_category: products['parent category'])
-#   end
-# end
-
-# products.each do |product|
-#   category = product['category']
-#   name = product['name']
-#   price = product['price']
-#   description = product['description']
-#   size = product['size']
-#   sku = product['sku']
-
-#   Category.where(name: category).first_or_create
-
-#   Product.create(name: name, current_price: price, size: size, description: description, sku: sku)
-# end
-
 products.each do |product|
   parent = product['parent category']
   sub_category = product['category']
