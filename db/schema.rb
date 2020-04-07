e5f5505e916892c1bca913a53c68ad25ba5501b3
jpk4 <jkleysen@academic.rrc.ca>
2020-04-07 02:19:03 -0500

    Add Page model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_034621) do
+ActiveRecord::Schema.define(version: 2020_04_07_071750) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -111,6 +111,14 @@ ActiveRecord::Schema.define(version: 2020_04_07_034621) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["customer_id"], name: "index_orders_on_customer_id"
     t.index ["payment_id"], name: "index_orders_on_payment_id"
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "permalink"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "payments", force: :cascade do |t|
