5a2e3bd341f03f692814c042b2befdde49f58e81
jpk4 <jkleysen@academic.rrc.ca>
2020-04-06 23:05:40 -0500

    Added Kaminari gem for pagination, added it to products show page

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,6 @@ class Product < ApplicationRecord
   validates :sku, :current_price, :name, presence: true
   validates :qty_on_hand, numericality: { only_integer: true }
   validates :current_price, format: { with: /\A\d+(?:\.\d{2})?\z/ }, numericality: { greater_than_or_equal_to: 0 }
+
+  paginates_per 24
 end
