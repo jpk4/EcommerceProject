8b52050be40f4257661d0de030fe8d58e314ead3
jpk4 <jkleysen@academic.rrc.ca>
2020-04-24 13:49:01 -0500

    Revert "Move everything associated to orders to join table order_products"
    This reverts commit c0a39a7e9979d152052c0d4ccfaaa582aebc9ec6.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,24 @@
 
 class OrdersController < ApplicationController
   def index; end
+
+  def new
+    if session[:cart].empty?
+      @cart = []
+    else
+      keys = session[:cart].keys
+      @cart = Product.find(keys)
+    end
+    @province = Province.find_by(code: 'MB')
+  end
+
+  def tax
+    @province = Province.find(params[:province_id])
+    if session[:cart].empty?
+      @cart = []
+    else
+      keys = session[:cart].keys
+      @cart = Product.find(keys)
+    end
+  end
 end
--- test/controllers/order_products_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class OrderProductsControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get order_products_show_url
-    assert_response :success
-  end
-
-end
